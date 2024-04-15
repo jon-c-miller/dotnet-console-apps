e6861d56afbada05d4dd0047c07a9a5270876e44
jon-c-miller <millerj.2203@protonmail.com>
2024-04-15 11:24:18 +0900

    program: remove unneeded usings

--- app/Program.cs
+++ app/Program.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-/* Developer Note: In the case of course changes, updating ChoiceToCourse() data is all that is necessary. Changes will be reflected throughout. */
-
-using ConsoleRegisterStudent;
+﻿using ConsoleRegisterStudent;
 
 class Program
 {
